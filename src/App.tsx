e981b69aabeb7c5987efa4fd9c44a644200c5296
Keisuke SATO <riaf@me.com>
2021-03-30 10:25:44 +0000

    vscode でコミットってこうするの？

---
+++ src/App.tsx
@@ -0,0 +1,48 @@
+import { FormControl, FormLabel } from "@chakra-ui/form-control";
+import { Input } from "@chakra-ui/input";
+import { Box, Container, Divider, Stack } from "@chakra-ui/layout";
+import React, { useState } from "react";
+import ColorPicker from "./components/ColorPicker";
+import Icon from "./components/Icon";
+
+function App() {
+  const [size, setSize] = useState(10);
+  const [color, setColor] = useState("#0099bd");
+  const [baseColor, setBaseColor] = useState("#000000");
+  const [bgColor, setBgColor] = useState("#ffffff");
+
+  return (
+    <Container bg={bgColor} p={10}>
+      <Box py={10}>
+        <Icon size={size} color={color} baseColor={baseColor} />
+      </Box>
+
+      <Stack spacing={3}>
+        <FormControl id="size">
+          <FormLabel>Size</FormLabel>
+          <Input
+            type="number"
+            value={size}
+            min={1}
+            onChange={(e) => setSize(parseInt(e.target.value, 10))}
+          />
+        </FormControl>
+        <FormControl id="color">
+          <FormLabel>Accent color</FormLabel>
+          <ColorPicker color={color} setColor={setColor} />
+        </FormControl>
+        <FormControl id="baseColor">
+          <FormLabel>Base text color</FormLabel>
+          <ColorPicker color={baseColor} setColor={setBaseColor} />
+        </FormControl>
+        <Divider />
+        <FormControl id="bgColor">
+          <FormLabel>Page background color</FormLabel>
+          <ColorPicker color={bgColor} setColor={setBgColor} />
+        </FormControl>
+      </Stack>
+    </Container>
+  );
+}
+
+export default App;
---
+++ src/components/Icon.tsx
@@ -0,0 +1,50 @@
+import { useEffect, useState } from "react";
+
+const Icon = ({ color = "#ff0000", size = 1, baseColor = "#000000" }) => {
+  const [image, setImage] = useState<string | null>(null);
+
+  useEffect(() => {
+    const canvasEl = document.createElement("canvas");
+    canvasEl.width = 16 * size;
+    canvasEl.height = 16 * size;
+
+    const ctx = canvasEl.getContext("2d");
+
+    if (ctx) {
+      ctx.clearRect(0, 0, 16, 16);
+
+      ctx.fillStyle = baseColor;
+      for (const rect of [
+        [0, 9, 2, 7],
+        [0, 7, 16, 2],
+        [6, 2, 2, 3],
+        [6, 9, 2, 7],
+        [8, 2, 1, 2],
+        [8, 14, 1, 2],
+        [9, 2, 2, 14],
+        [12, 0, 2, 16],
+        [14, 0, 2, 2],
+      ]) {
+        const [x, y, w, h] = rect.map((i) => i * size);
+        ctx.fillRect(x, y, w, h);
+      }
+
+      ctx.fillStyle = color;
+      for (const rect of [
+        [3, 4, 2, 2],
+        [3, 10, 2, 6],
+      ]) {
+        const [x, y, w, h] = rect.map((i) => i * size);
+        ctx.fillRect(x, y, w, h);
+      }
+
+      setImage(canvasEl.toDataURL());
+    }
+  }, [color, size, baseColor]);
+
+  return (
+    <>{image && <img src={image} width={16 * size} height={16 * size} />}</>
+  );
+};
+
+export default Icon;
